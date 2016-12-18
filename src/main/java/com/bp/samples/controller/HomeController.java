74d6c0b9ecfeefc43f7d9b8184907728b0b00ceb
Pirvali, Behzad <bpirvali@gmail.com>
2016-12-18 11:57:58 -0800

    made it a working sample of bootstrap with list and tables

--- src/main/java/com/bp/samples/controller/HomeController.java
+++ src/main/java/com/bp/samples/controller/HomeController.java
@@ -1,9 +1,7 @@
 package com.bp.samples.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by behzad.pirvali on 12/5/16.
@@ -11,19 +9,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
-    public String index(
-            @RequestParam(value="name", required=false, defaultValue="User") String name,
-            Model model) {
-        model.addAttribute("name", name);
-        return "index";
-    }
+    @RequestMapping(value = {"/", "index", "index.html"})
+    public String index() { return "index"; }
+
+    @RequestMapping(value = {"about", "about.html"})
+    public String about() { return "about"; }
+
+    @RequestMapping(value = {"contact", "contact.html"})
+    public String contact() { return "contact"; }
+
     @RequestMapping(value = "/bootstrap-test")
-    public String bootstrapTest(
-            @RequestParam(value="name", required=false, defaultValue="User") String name,
-            Model model) {
-        if (name!=null)
-            model.addAttribute("name", name);
-        return "bootstrap-test";
-    }
+    public String bootstrapTest() { return "bootstrap-test"; }
+
 }
